8df93c919f8256ad95174e81de44bed12d356cd3
secretmoo35 <sarayut-moo.th@hotmail.com>
2016-04-05 14:09:21 +0700

    dashboard

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,14 +2,18 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, $location) {
 
-
+    $scope.loginMenu = "";
     $scope.login = function() {
         $location.path('/app/dashboard');
+        $scope.loginMenu = true;
     }
-    $scope.register = function(){
-    	$location.path('/register');	
+    $scope.register = function() {
+        $location.path('/register');
     }
-        $scope.gotologin = function(){
-    	$location.path('/app/login');	
+    $scope.gotologin = function() {
+        $location.path('/app/login');
+    }
+    $scope.menuLogin = function() {
+        $scope.loginMenu;
     }
 })
