76fa7a66eabc2ec6f61126b4fe1a597e798c979a
tingtang35 <tingtang35@gmail.com>
2016-04-06 12:25:08 +0700

    update

--- www/js/controller/evalucontroller.js
+++ www/js/controller/evalucontroller.js
@@ -1,7 +1,7 @@
 myApp.controller('EvaluCtrl', function($scope, $ionicModal, $timeout, $location, $http,$state,$stateParams) {
 
 	$scope.ans = "s";
-    $scope.answers = { "question ": "คุณอายุเท่าไหร่", "answer": ["ก. 20 - 30","ข. 31 - 40","ค. 41 - 50","ง. 51 ขึ้นไป"] },
+    $scope.answers = { "question ": "1.คุณอายุเท่าไหร่", "answer": ["ก. 20 - 30","ข. 31 - 40","ค. 41 - 50","ง. 51 ขึ้นไป"] },
     
     $scope.selectAns = function(ans){
     	$scope.ans = ans;
